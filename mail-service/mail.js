91b72daf05167a8e314c45c74aa4dd6827cd8b39
gattex <damgattesco@gmail.com>
2016-11-30 09:08:32 -0300

    saco express, no hay service rest

--- mail-service/mail.js
+++ mail-service/mail.js
@@ -1,14 +1,5 @@
-var express = require('express');
-var app = express();
 var rabbit = require('./rabbitMQ');
 
-app.post('/mail/send', function (req, res, next) {
-	res.send("Mail Sent");
-});
-
-app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
-});
 
 process.on('uncaughtException', function (err) {
 	console.error(err);
